7c67089b2d771ba59402d9222eaff62093782f77
Matan Amoyal <matanamoyal@matans-mac.local>
2018-06-01 21:40:07 +0300

    added button permissions

--- unknown-software-app/ViewController.swift
+++ unknown-software-app/ViewController.swift
@@ -13,10 +13,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if FBSDKAccessToken.current() != nil {
+            // User is logged in, do work such as go to next view controller.
+        }
         // Do any additional setup after loading the view, typically from a nib.
         let loginButton = FBSDKLoginButton()
         // Optional: Place the button in the center of your view.
         loginButton.center = view.center
+        loginButton.readPermissions = ["public_profile", "email"]
         view.addSubview(loginButton)
     }
 
